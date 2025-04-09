e4cc8e85046d147717915dc634282a4e0c403f25
xuannguyen-karros <xuan.nguyen@karrostech.com>
2025-04-09 14:09:40 +0700

    module

---
+++ vpc/examples/manage-default-vpc/main.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  region = local.region
+}
+
+locals {
+  name   = "ex-${replace(basename(path.cwd), "_", "-")}"
+  region = "eu-west-1"
+
+  tags = {
+    Example    = local.name
+    GithubRepo = "terraform-aws-vpc"
+    GithubOrg  = "terraform-aws-modules"
+  }
+}
+
+################################################################################
+# VPC Module
+################################################################################
+
+module "vpc" {
+  source = "../../"
+
+  create_vpc = false
+
+  manage_default_vpc               = true
+  default_vpc_name                 = "default"
+  default_vpc_enable_dns_hostnames = true
+
+  tags = local.tags
+}
